e303f07f79f3723864c501f791824f486c72117d
Macedo Francisco <macedolb53@gmail.com>
2024-04-08 01:19:42 +0200

    fone

---
+++ AulaPraticas/js/menuMobile.js
@@ -0,0 +1,10 @@
+const menuMobile = document.querySelector(".navBarLinks")
+
+
+function mostraMenu(){
+    if(menuMobile.style.display == "block"){
+        menuMobile.style.display = "none";
+    }else{
+        menuMobile.style.display = "black";
+    }
+}
